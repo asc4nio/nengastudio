cf215cf0e9562c2e0905622c9d7bd6951e5016c5
asc4nio <ascaniopaonetti@gmail.com>
2023-06-11 14:43:05 +0200

    proto-a deep clean

--- prototype/proto-a/main.js
+++ prototype/proto-a/main.js
@@ -1,7 +1,49 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { DecalGeometry } from "three/addons/geometries/DecalGeometry.js";
+// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+// import { DecalGeometry } from "three/addons/geometries/DecalGeometry.js";
 
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // fitCamera();
+  // renderer.setSize(container.offsetWidth, container.offsetHeight);
+  // renderer.setPixelRatio(window.devicePixelRatio);
+}
+window.addEventListener("resize", onWindowResize);
+
+/*
 let container, camera, scene, renderer;
 let mesh;
 
@@ -71,3 +113,4 @@ function onWindowResize() {
 window.addEventListener("resize", onWindowResize);
 
 init();
+*/
